280e836ccddd937b30614ebd7103167f5572e25d
Mahad Kalam <55807755+SkyfallWasTaken@users.noreply.github.com>
2024-11-11 10:27:44 +0000

    Update instrument.js

--- instrument.js
+++ instrument.js
@@ -1,11 +1,5 @@
-const Sentry = require("@sentry/node");
-const { nodeProfilingIntegration } = require("@sentry/profiling-node");
-
+import * as Sentry from "@sentry/bun";
 Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  integrations: [
-    nodeProfilingIntegration(),
-  ],
-  tracesSampleRate: 1.0,
-  profilesSampleRate: 1.0,
+    dsn: process.env.SENTRY_DSN,
+    tracesSampleRate: 1.0,
 });
